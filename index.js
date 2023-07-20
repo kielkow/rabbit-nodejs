3d7115c0cf755b2c55c5acaeb04a4a2b8fd98918
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2023-07-20 15:34:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Consume RabbitMQ data by API
+// URL: localhost:5672/api/index.html
+// Examples:
+// Get Hosts: localhost:5672/api/queues/vhosts
+// Get Queue Data: localhost:5672/api/queues/<vhost_name>/<queue_name>
+
+const axios = require('axios');
+const amqplib = require('amqplib');
 const amqp = require('amqplib/callback_api');
 
 /**
@@ -7,18 +15,20 @@ const amqp = require('amqplib/callback_api');
  */
 class RabbitMQ {
     /**
-    * Create define amqpUrl.
-    * @param {string} amqpUrl - Advanced Message Queuing Protocol Rabbit string
-    */
-    constructor({ amqpUrl }) {
+     * Create define amqpUrl.
+     * @param {string} amqpUrl - Advanced Message Queuing Protocol Rabbit string
+     */
+    constructor({
+        amqpUrl
+    }) {
         this.amqpUrl = amqpUrl
         this.connection = undefined
         this.channel = undefined
     }
     /**
-    * Create a connections.
-    * @returns {Promise} Promise object represents rabbitMQ connection.
-    */
+     * Create a connections.
+     * @returns {Promise} Promise object represents rabbitMQ connection.
+     */
     async connect() {
         this.connection = await new Promise((resolve, reject) => {
             amqp.connect(this.amqpUrl, (error, connection) => {
@@ -30,29 +40,31 @@ class RabbitMQ {
         })
     }
     /**
-    * Create a confirm channel.
-    * @returns {Promise} Promise object represents rabbitMQ channel.
-    */
+     * Create a confirm channel.
+     * @returns {Promise} Promise object represents rabbitMQ channel.
+     */
     createConfirmChannel() {
         return new Promise((resolve, reject) => {
             this.connection.createConfirmChannel((error, channel) => {
                 if (error) {
                     return reject(error);
-                } return resolve(channel);
+                }
+                return resolve(channel);
             })
         })
     }
 
     /**
-    * Create a channel.
-    * @returns {Promise} Promise object represents rabbitMQ channel.
-    */
+     * Create a channel.
+     * @returns {Promise} Promise object represents rabbitMQ channel.
+     */
     createChannel() {
         return new Promise((resolve, reject) => {
             this.connection.createChannel((error, channel) => {
                 if (error) {
                     return reject(error);
-                } return resolve(channel);
+                }
+                return resolve(channel);
             })
         })
     }
@@ -62,7 +74,10 @@ class RabbitMQ {
      * @param {Object} data - data to send to queue
      * @returns {Promise} Promise Boolean as long as successful.
      */
-    async sendToQueue({ queueName, data }) {
+    async sendToQueue({
+        queueName,
+        data
+    }) {
         if (!this.connection) {
             throw `rabbitMQ's instance is not connected! `
         }
@@ -89,12 +104,16 @@ class RabbitMQ {
         })
     }
 
-    async consumeQueue({ queueName }) {
+    async consumeQueue({
+        queueName
+    }) {
         if (!this.connection) throw `rabbitMQ's instance is not connected! `
 
         if (!this.channel) this.channel = await this.createChannel();
 
-        this.channel.assertQueue(queueName, { durable: true });
+        this.channel.assertQueue(queueName, {
+            durable: true
+        });
         const messages = []
 
         this.channel.consume(
@@ -103,14 +122,17 @@ class RabbitMQ {
                 if (msg !== null) {
                     messages.push(JSON.parse(msg.content.toString()));
                 }
-            },
-            { noAck: true }
+            }, {
+                noAck: true
+            }
         );
 
         return messages;
     }
 
-    async getMessageCount({ queueName }) {
+    async getMessageCount({
+        queueName
+    }) {
         if (!this.connection) {
             throw `rabbitMQ's instance is not connected! `
         }
@@ -119,7 +141,9 @@ class RabbitMQ {
             this.channel = await this.createChannel();
         }
         return new Promise((resolve, reject) => {
-            this.channel.assertQueue(queueName, { durable: true }, (err, data) => {
+            this.channel.assertQueue(queueName, {
+                durable: true
+            }, (err, data) => {
                 if (err) reject(err)
                 resolve(data.messageCount)
             });
@@ -141,6 +165,60 @@ class RabbitMQ {
         if (this.channel) {
             await this.channel.close()
         }
+    }
+
+    testConn() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const connection = await amqplib.connect(process.env.RABBIT_URL);
+
+                const channel = await connection.createChannel();
+
+                await channel.assertQueue('test-conn');
+
+                console.log('[RABBITMQ]: CONNECTION SUCCESS');
+
+                channel.close();
+                connection.close();
+
+                return resolve();
+            } catch (error) {
+                console.error('[RABBITMQ]: CONNECTION FAIL\n', error);
+                return reject(error);
+            }
+        });
+    }
+
+    getQueueInfo(queue) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const {
+                    data
+                } = await axios.get(
+                    `localhost:5672/api/queues/${process.env.RABBIT_USER}/${queue}`, {
+                        auth: {
+                            username: process.env.RABBIT_USER,
+                            password: process.env.RABBIT_PASS,
+                        },
+                    }
+                );
+
+                console.log(
+                    'QUEUE INFO', {
+                        messageCount: data.messages,
+                        consumerCount: data.consumers,
+                    }
+                );
+
+                return resolve({
+                    messageCount: data.messages,
+                    consumerCount: data.consumers,
+                });
+            } catch (error) {
+                console.error('[RABBITMQ]: GET QUEUE INFO FAIL\n', error);
+                return reject(error);
+            }
+        });
     }
 }
 
@@ -164,8 +242,7 @@ module.exports = async (ctx) => {
             status: 200,
             message: "Message sended with success!"
         }
-    }
-    catch (error) {
+    } catch (error) {
         return {
             status: 500,
             message: error.message || error,
